c4be39a4d2821dfbd851ef335986432701e7c4f7
Ansar Ismagilov <ansar89kab@bk.ru>
2023-03-29 14:14:46 +0700

    creating Personage, Hero

--- src/Hero.java
+++ src/Hero.java
@@ -6,5 +6,10 @@ public class Hero extends Personage{
     public Hero(String name, int health, int gold, int skill, int power, int experience) {
         super(name, health, gold, skill, power, experience);
         mixtures = new HashMap<>();
+        mixtures.put("10",10);
+        mixtures.put("20",10);
+        mixtures.put("30",10);
+        mixtures.put("40",10);
+        mixtures.put("10",10);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         var bob = new Hero("Bob",100, 100, 100, 100, 1000);
-
+        System.out.println(bob.mixtures.keySet());
     }
 }
